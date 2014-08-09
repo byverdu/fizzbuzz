b72c3d762e33df2d7e139562aaa7a55e1cdcd5c0
Arve80 <alvaltor@gmail.com>
2014-08-10 00:21:38 +0100

    test divisible by 3 passed

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,3 @@
+def is_divisible_by_three?(number)
+	number % 3 == 0
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,15 @@
 require 'fizzbuzz'
 
 describe 'Fizzbuzz' do
+
+	it "Should be divisible by 3" do
+
+		expect(is_divisible_by_three?(3)).to be true
+	end
+
+	it "Should not be divisible by 3" do
+
+		expect(is_divisible_by_three?(1)).not_to be true
+	end
 	
 end
