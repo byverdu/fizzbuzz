fb3efef6c6163834ba622f29dfea3ef0fa9814a9
Arve80 <alvaltor@gmail.com>
2014-08-10 00:22:17 +0100

    test divisible by 3 passed

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -11,5 +11,10 @@ describe 'Fizzbuzz' do
 
 		expect(is_divisible_by_three?(1)).not_to be true
 	end
+
+	it "Should be divisible by 5" do
+
+		expect(is_divisible_by_five?(5)).to be true
+	end
 	
 end
