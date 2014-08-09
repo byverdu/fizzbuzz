893660bd70a95ebb09e16884cc02d9bc0b1255ff
Arve80 <alvaltor@gmail.com>
2014-08-10 00:27:34 +0100

    test divisible by 5 passed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,7 @@
 def is_divisible_by_three?(number)
 	number % 3 == 0
 end
+
+def is_divisible_by_five?(number)
+	number % 5 == 0
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,8 @@ require 'fizzbuzz'
 
 describe 'Fizzbuzz' do
 
+	# Divisible by 3 case
+
 	it "Should be divisible by 3" do
 
 		expect(is_divisible_by_three?(3)).to be true
@@ -12,9 +14,20 @@ describe 'Fizzbuzz' do
 		expect(is_divisible_by_three?(1)).not_to be true
 	end
 
+	# Divisible by 5 case
+
 	it "Should be divisible by 5" do
 
 		expect(is_divisible_by_five?(5)).to be true
 	end
+
+	it "Should not be divisible by 5" do
+
+		expect(is_divisible_by_five?(1)).not_to be true
+	end
+
+	# Divisible by 15 case
+
+	
 	
 end
